79e54fdc0199fa046330486392d7648125e17989
Shawngxw <shawngxw@yahoo.com>
2022-12-16 13:21:44 +0800

    init

--- crawler/README.md
+++ crawler/README.md
@@ -1,3 +1,3 @@
-1. 安装[python](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe) 
+1. 安装[python](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe):https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe
 2. 终端执行：pip install --no-cache-dir -r requirements.txt
-3. python database.py
+3. python main.py
--- email/index.js
+++ email/index.js
@@ -1,54 +1,55 @@
 require('dotenv').config({path: '.env'})
 const axios = require("axios")
-const {config} = require("dotenv")
 const dayjs = require("dayjs")
 
 ;const {sendEmail} = require("./utils");
 (async () => {
-    const response = await axios.get("http://localhost:8000/db", {
-        params: {
-            collection: "time",
-            _id: "lastRunTime"
-        }
-    })
-    const createTime = response.data.data[0].createTime
-    // 判断启动时间是否大于延迟监听时间
-    if ((dayjs().unix() - createTime) / 60 / 60 >= process.env.DELAY_TIME_SPAN) {
-        const response = await axios.post("http://localhost:8000/db/s",
-            {createTime: {$gt: dayjs().unix() - process.env.MONITOR_TIME_SPAN * 60 * 60}}
-        )
-        const {data} = response.data
-        const targets = data.map(records => {
-            const _id = records._id
-            // 根据时间升序
-            const _records = Object.entries(records).filter(record => record[0].startsWith("16")).map(record => {
-                record[0] = parseInt(record[0])
-                return record
-            }).sort((a, b) => a[0] - b[0])
-            const {isSendEmail} = records
-            // 取第一个和最后一个
-            const initialScore = _records[0][1].score
-            const currentScore = _records.pop()[1].score
-            const extremum = initialScore - currentScore
-            return {
-                _id, initialScore, currentScore, extremum, isSendEmail
-            }
-        })
-        const finallyResults = targets
-            // 阈值大于设定值
-            .filter(target => {
-                return Math.abs(target.extremum) >= process.env.THRESHOLD
+    while (true) {
+        try {
+            const response = await axios.get("http://localhost:8000/db", {
+                params: {
+                    collection: "time",
+                    _id: "lastRunTime"
+                }
             })
-            // 过滤初始值为正增加和初始值为负减少
-            // .filter(target => {
-            //     return !(target.initialScore > 0 && target.currentScore > target.initialScore ||
-            //         target.initialScore < 0 && target.currentScore < target.initialScore
-            //     )
-            // })
-            .filter(target => {
-                return !target.isSendEmail
-            })
-        const displayResult = finallyResults.map(finallyResult => `
+            const createTime = response.data.data[0].createTime
+            // 判断启动时间是否大于延迟监听时间
+            if ((dayjs().unix() - createTime) / 60 / 60 >= process.env.DELAY_TIME_SPAN) {
+                const response = await axios.post("http://localhost:8000/db/s",
+                    {createTime: {$gt: dayjs().unix() - process.env.MONITOR_TIME_SPAN * 60 * 60}}
+                )
+                const {data} = response.data
+                const targets = data.map(records => {
+                    const _id = records._id
+                    // 根据时间升序
+                    const _records = Object.entries(records).filter(record => record[0].startsWith("16")).map(record => {
+                        record[0] = parseInt(record[0])
+                        return record
+                    }).sort((a, b) => a[0] - b[0])
+                    const {isSendEmail} = records
+                    // 取第一个和最后一个
+                    const initialScore = _records[0][1].score
+                    const currentScore = _records.pop()[1].score
+                    const extremum = initialScore - currentScore
+                    return {
+                        _id, initialScore, currentScore, extremum, isSendEmail
+                    }
+                })
+                const finallyResults = targets
+                    // 阈值大于设定值
+                    .filter(target => {
+                        return Math.abs(target.extremum) >= process.env.THRESHOLD
+                    })
+                    // 过滤初始值为正增加和初始值为负减少
+                    // .filter(target => {
+                    //     return !(target.initialScore > 0 && target.currentScore > target.initialScore ||
+                    //         target.initialScore < 0 && target.currentScore < target.initialScore
+                    //     )
+                    // })
+                    .filter(target => {
+                        return !target.isSendEmail
+                    })
+                const displayResult = finallyResults.map(finallyResult => `
             <h1>${finallyResult._id}</h1>
             <div style="display: flex"><div style="font-weight: bold;">${finallyResult.initialScore > 0 ? "主队" : "客队"}初始让分值:</div><div>${Math.abs(finallyResult.initialScore)}</div></div>
             <div style="display: flex"><div style="font-weight: bold;">${finallyResult.currentScore > 0 ? "主队" : "客队"}当前让分值:</div><div>${Math.abs(finallyResult.currentScore)}</div></div>
@@ -57,19 +58,25 @@ const dayjs = require("dayjs")
             <hr/>
         `).join("")
 
-        if (finallyResults.length) {
-            await sendEmail({
-                to: process.env.RECEIVE_EMAIL || process.env.EMAIL_USER,
-                subject: "篮球实时数据通知",
-                content: displayResult
-            })
+                if (finallyResults.length) {
+                    await sendEmail({
+                        to: process.env.RECEIVE_EMAIL || process.env.EMAIL_USER,
+                        subject: "篮球实时数据通知",
+                        content: displayResult
+                    })
+                }
+                finallyResults.forEach(result => {
+                    axios.put("http://localhost:8000/db", {
+                        _id: result._id,
+                        isSendEmail: true
+                    })
+                })
+            }
+        } catch (e) {
+            console.log(e)
         }
-        finallyResults.forEach(result => {
-            axios.put("http://localhost:8000/db", {
-                _id: result._id,
-                isSendEmail: true
-            })
-        })
+
     }
+
 
 })()
