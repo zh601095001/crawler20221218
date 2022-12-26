1a02720e86e76b4caca86d0c31921e4a0d71084a
zhaohao <601095001@qq.com>
2022-12-26 19:49:48 +0800

    修改了解析源码

--- email/test.js
+++
@@ -1,4 +0,0 @@
-const str = '<span class="odds2 ">8.5</span><span class="odds1 ">0.88</span>'
-
-const res = str.match(/^<span class="odds2.*?">(.*?)<\/span>.*?$/)
-console.log(res)
--- email/test.py
+++
@@ -1,7 +0,0 @@
-import time
-from datetime import datetime
-
-import pytz
-
-t = datetime.fromtimestamp(1671263737, pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
-print(t)
